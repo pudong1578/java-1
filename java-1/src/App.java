f02a30fe227ae425a259f8173bd62a3ce345a4a6
pudong1578 <aksrmsdl8452@gmail.com>
2025-09-18 16:48:16 +0900

    09_18

--- java-1/src/App.java
+++ java-1/src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.err.print("정수를 입력하세요 : ");
+        int s = scanner.nextInt();
+        int m = s / 60;
+        s %= 60;
+        int h = m / 60;
+        m %= 60;
+
+        System.err.printf("%d시간 %d분 %d초", h, m, s);
     }
 }

7d54898a13ea9eec1a43756f8aba4d8e49b7d834
pudong1578 <aksrmsdl8452@gmail.com>
2025-09-25 15:50:51 +0900

    계절 분기

--- java-1/src/App.java
+++ java-1/src/App.java
@@ -2,8 +2,16 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        int a= 33, b=13;
-        int big = (a>b) ? a:b;
-        System.out.println("big = " + big);
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        if(m >= 3 && m <= 5){
+            System.out.println("봄");
+        }else if(m >= 6 && m <= 8){
+            System.out.println("여름");
+        }else if(m >= 9 && m <= 11){
+            System.out.println("가을");
+        }else if(m >= 12 || m <= 2){
+            System.out.println("겨울");
+        }
     }
 }

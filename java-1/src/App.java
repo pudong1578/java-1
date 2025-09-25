496cc6d6e727c56b847d20ccc47c6dcde55ffacc
pudong1578 <aksrmsdl8452@gmail.com>
2025-09-25 14:51:53 +0900

    크기비교

--- java-1/src/App.java
+++ java-1/src/App.java
@@ -2,16 +2,8 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        int a=3, b=3, c=3;
-        a += b;
-        System.out.println(a+" / "+b);
-        a-=b;
-        System.out.println(a+" / "+b);
-        b = a++;
-        System.out.println(a+" / "+b);
-        a = ++b;
-        System.out.println(b+" / "+a);
-        a = c++;
-        System.out.println(a+" / "+b);
+        int a= 33, b=13;
+        int big = (a>b) ? a:b;
+        System.out.println("big = " + big);
     }
 }

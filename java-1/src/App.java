ded2b03abb0479319fde38b19b7661ec0cb72dc4
pudong1578 <aksrmsdl8452@gmail.com>
2025-10-02 14:22:49 +0900

    while tesst [10_02]

--- java-1/src/App.java
+++ java-1/src/App.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int m = scan.nextInt();
-        if(m >= 3 && m <= 5){
-            System.out.println("봄");
-        }else if(m >= 6 && m <= 8){
-            System.out.println("여름");
-        }else if(m >= 9 && m <= 11){
-            System.out.println("가을");
-        }else if(m >= 12 || m <= 2){
-            System.out.println("겨울");
+        System.out.println("정수 5개를 입력하세요.");
+        
+        while (true) {
+            System.out.print(">>");
+            String text = scan.nextLine();
+            if(text.equals("exit")) {break;}
         }
+
+        System.out.println("종료합니다...");
+        scan.close();
     }
 }

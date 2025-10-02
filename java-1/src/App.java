b8337f3fed457428769526bb98972be7242364a4
pudong1578 <aksrmsdl8452@gmail.com>
2025-10-02 15:09:36 +0900

    평균 구하기 [10_02]

--- java-1/src/App.java
+++ java-1/src/App.java
@@ -7,16 +7,18 @@ public class App {
         int intArr[];
         intArr = new int[5];
 
-        int max = 0;
         System.out.println("양수 5개를 입력하세요.");
 
         for(int i = 0 ; i < 5; i++){
             intArr[i] = scan.nextInt();
-            if(intArr[i] > max){
-                max = intArr[i];
-            }
+            
         }
-        System.out.println("가장 큰 수는 " + max + "입니다.");
+        double sum = 0.0;
+        for(int i = 0; i < 5; i++){
+            sum += intArr[i];
+        }
+        
+        System.out.println("평균은 " + sum / 5);
         scan.close();
     }
 }

3bc6aab4711b6491f6bd70e3ac2f09b641aa8cbd
pudong1578 <aksrmsdl8452@gmail.com>
2025-10-02 16:18:54 +0900

    try catch [10_02]

--- java-1/src/App.java
+++ java-1/src/App.java
@@ -1,19 +1,25 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
-    static int[] makeArray(){
-        int temp[] = new int[4];
-        for(int i = 0; i < 4; i++){
-            temp[i] = i;
-        }
-        return temp;
-    }
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int intArray[] = makeArray();
-        for(int i = 0; i < 4; i++){
-            System.out.print(intArray[i] + " ");
+        System.out.println("정수3개를 입력하세요");
+        int sum=0,n=0;
+        for(int i =0; i<3;i++){
+            System.out.print(">>");
+            try{
+                n = scan.nextInt();
+            }
+            catch(InputMismatchException a){
+                System.out.println("정수가 아닙니다 다시 입력하세요!");
+                scan.next();
+                i--;
+                continue;
+            }
+            sum += n;
         }
+        System.out.println("합은 " + sum);
         scan.close();
     }
 }
